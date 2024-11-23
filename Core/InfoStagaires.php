0df0062908259a01ef798cda6f1382aaddc56375
otmane <otmanotchelhanan@gmail.com>
2024-11-23 17:26:47 +0100

    Delete updateS and create file for get info for allstagaires

--- Core/InfoStagaires.php
+++ Core/InfoStagaires.php
@@ -5,7 +5,7 @@ use Core\Database;
 $db = App::resolve(Database::class);
 $filiaire = $_POST['filiere']?? '';
 $annee_etude = $_POST['annee_etude']?? '';
-$db->query('SELECT * INTO stagiaires WHERE filiere=:filiere And annee_etude=:annee_etude', compact('filiaire', 'annee_etude')).findAll();
+$db->query('SELECT * INTO stagiaires WHERE filiere=:filiere And annee_etude=:annee_etude', compact('filiaire', 'annee_etude'))->findAll();
 
 
 
