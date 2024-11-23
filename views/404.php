0a074e3c1738ebfca0dc3bbe147102182282bb53
KamalOS <noreply@kamal.com>
2024-11-23 14:16:38 +0100

    Update 404.php

--- views/404.php
+++ views/404.php
@@ -1,5 +1,4 @@
 <?php require base_path('views/partials/head.view.php'); ?>
-<?php require base_path('views/partials/sidebar.view.php'); ?>
 
 <h1>Desolé page n'existe pas</h1>
 <a href="/">Retourner à la page d'accueil</a>
