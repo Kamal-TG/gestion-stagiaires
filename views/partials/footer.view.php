11ca87abb1ab04774370e0efeebb9a8c52e83e5c
mohamed <mohamedsolayman929@gmail.com>
2024-11-25 21:45:20 +0100

    add partials

--- views/partials/footer.view.php
+++ views/partials/footer.view.php
@@ -1,4 +1,12 @@
-<!-- Footer here -->
+</main>
+</div>
+</div>
+
+<div class="bg-body-tertiary">
+    <footer class="p-0 m-0 py-3">
+        <p class="m-0 text-center text-muted">© 2024 Company, Inc</p>
+    </footer>
+</div>
 
 </body>
 
