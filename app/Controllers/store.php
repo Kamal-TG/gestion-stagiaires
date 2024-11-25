2fadc50d18bfb8aa302a402b3fe968d8179c7cb0
Kamal-TG <doergoodnes@gmail.com>
2024-11-25 20:31:55 +0000

    add code to store and update a trainee

--- app/Controllers/store.php
+++ app/Controllers/store.php
@@ -1,8 +1,8 @@
 <?php
-// Ajouter student
+
 use Core\App;
 use Core\Database;
-$db = App::resolve(Database::class);
+
 $nom = $_POST['nom'] ?? '';
 $prenom = $_POST['prenom'] ?? '';
 $filiere = $_POST['filiere'] ?? '';
@@ -10,7 +10,7 @@ $anne_etude = $_POST['annee_etude'] ?? '';
 $type_bac = $_POST['type_bac'] ?? '';
 $annee_bac = $_POST['annee_bac'] ?? '';
 
-$db->query(
+App::resolve(Database::class)->query(
     "INSERT INTO stagiaires
     VALUES(DEFAULT, :nom, :prenom, :filiere, :annee_etude, :type_bac, :annee_bac)",
     compact('nom', 'prenom', 'filiere', 'anne_etude', 'type_bac', 'annee_bac')
