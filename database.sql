baa78724d4b49de615ab8ad276b5b4c545ea1aff
KamalOS <noreply@kamal.com>
2024-11-28 14:19:17 +0100

    update database to add absences table

--- database.sql
+++ database.sql
@@ -13,3 +13,12 @@ CREATE TABLE stagiaires (
     type_bac VARCHAR(50) NOT NULL,
     annee_bac VARCHAR(4) NOT NULL
 );
+
+CREATE TABLE absences (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre_heures INT NOT NULL,
+    date_absence DATE NOT NULL,
+    justifie BOOLEAN NOT NULL,
+    id_stagiaire INT NOT NULL,
+    FOREIGN KEY (id_stagiaire) REFERENCES stagiaires(id)
+)
